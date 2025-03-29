6a62192439fa693c38c6f26d48d25c1f34046214
ShirleyXW <shirley.wang.xl@outlook.com>
2025-03-29 03:18:33 -0700

    add ImageDisplay and Accessory components

--- frontend/app/routes/home.tsx
+++ frontend/app/routes/home.tsx
@@ -1,5 +1,7 @@
 import type { Route } from "./+types/home";
-
+import ImageDisplay from "app/components/image_display"
+import Accessory from "~/components/accessory";
+import {useState} from "react";
 export function meta({}: Route.MetaArgs) {
   return [
     { title: "New React Router App" },
@@ -8,7 +10,12 @@ export function meta({}: Route.MetaArgs) {
 }
 
 export default function Home() {
+    const [selectedAccessory, setSelectedAccessory] = useState<string | null>(null);
   return (
-      <p>Home</p>
+      <div
+          className="bg-gradient-to-br from-[#ffb627] via-[#fad643] to-[#ff5c8a] bg-black w-full h-screen flex flex-col items-center">
+          <ImageDisplay selectedAccessory={selectedAccessory}/>
+          <Accessory onSelect={setSelectedAccessory}/>
+      </div>
   );
 }
