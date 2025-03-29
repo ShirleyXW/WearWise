ad62cee541f1b6cb7a184d2038c13b1899f9da30
ShirleyXW <shirley.wang.xl@outlook.com>
2025-03-29 03:17:42 -0700

    create Accessory component

---
+++ frontend/app/components/accessory.tsx
@@ -0,0 +1,46 @@
+import {
+    Carousel,
+    CarouselContent,
+    CarouselItem,
+    CarouselNext,
+    CarouselPrevious,
+} from "~/components/ui/carousel"
+import {useState} from "react";
+import {Card, CardContent} from "~/components/ui/card";
+import acc3Url from "/assets/glasses/acc3.png";
+import acc4Url from "/assets/glasses/acc4.png";
+import acc5Url from "/assets/glasses/acc5.png";
+import acc6Url from "/assets/glasses/acc6.png";
+import acc7Url from "/assets/glasses/acc7.png";
+
+
+const Accessory = ({ onSelect }: { onSelect: (imgUrl: string | null) => void }) => {
+    const accessories = [acc3Url, acc4Url, acc5Url, acc6Url, acc7Url];
+    const [selectedAccessory, setSelectedAccessory] = useState<string | null>(null);
+    const wearAccessory = (imgUrl:string) => {
+        const newSelection = selectedAccessory === imgUrl ? null : imgUrl;
+        setSelectedAccessory(newSelection);
+        onSelect(newSelection);
+    }
+    return (
+        <Carousel className="w-1/3 h-1/3">
+            <CarouselContent className="-ml-1 h-full">
+                {accessories.map((url, index) => (
+                    <CarouselItem key={index} className="pl-1 md:basis-1/2 lg:basis-1/3">
+                        <div className="p-1">
+                            <Card>
+                                <CardContent className="flex aspect-square items-center justify-center p-6"
+                                             onClick={() => wearAccessory(url)}>
+                                    <img className="text-2xl font-semibold" src={url}  alt={`accessory_${index}`}/>
+                                </CardContent>
+                            </Card>
+                        </div>
+                    </CarouselItem>
+                ))}
+            </CarouselContent>
+            <CarouselPrevious/>
+            <CarouselNext/>
+        </Carousel>
+    )
+}
+export default Accessory
