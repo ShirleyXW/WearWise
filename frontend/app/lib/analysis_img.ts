02e38683eb2107dff3ef5b3656af9f0e176d6cdd
ShirleyXW <shirley.wang.xl@outlook.com>
2025-04-08 20:56:18 -0700

    feat: auto switch API URL based on environment

--- frontend/app/lib/analysis_img.ts
+++ frontend/app/lib/analysis_img.ts
@@ -1,8 +1,13 @@
 import axios from 'axios';
 
+const isLocal = window.location.hostname === "localhost";
+const deployedURL = isLocal
+  ? "http://localhost:8000"
+  : "https://wearwise-utof.onrender.com";
+
 export const analysisImg = async (formData: FormData, config = {}) => {
     try {
-        const response = await axios.post("http://localhost:8000/predict", formData, {
+        const response = await axios.post(`${deployedURL}/predict`, formData, {
             ...config,
             headers: {
                 "Content-Type": "multipart/form-data",
